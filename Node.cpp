c17b41066840f33b4396c53c0996bf5eecd6af7e
TimothyLangi <48646175+TimothyLangi@users.noreply.github.com>
2019-05-22 16:11:49 +1000

    Add files via upload

--- Node.cpp
+++ Node.cpp
@@ -3,19 +3,13 @@
 
 Node::Node(Tile* t, Node* n)
 {
-   // struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
-   // tile->colour = tile->colour;
-   // tile->shape = tile->shape;
    tile = t;
    next = n;
-   // TODO
 }
 
 Node::Node(Node& other)
 {
-   // struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
    next = other.next;
    tile->colour = other.tile->colour;
    tile->shape = other.tile->shape;
-   // TODO
 }
